13dfd6a22abac50244b28992abf21ed78acbca9f
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-06-03 11:52:04 -0300

    feat: add new app

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls'))
+    path('', include('example_app.urls')),
+    path('/projects', include('projects.urls'))
 ]

0d4ddd217bfb33132dcf1987f4c957be5bebb51f
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-06-03 23:58:44 -0300

    feat: integrate projects page and page details

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from projects import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>', views.project_detail, name='project_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from projects.models import Project
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'projects.html', {})
+    projects = Project.objects.all()  # get all projects
+    context = {
+        'projects': projects
+    }
+    return render(request, 'projects.html', context)
+
+
+def project_detail(request, id):
+    project = Project.objects.get(id=id)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
